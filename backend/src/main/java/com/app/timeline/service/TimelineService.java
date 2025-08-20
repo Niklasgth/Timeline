964ec4cc8f31be0f8c89a2ff006f223f90b912cc
Niklas <Niklasgth@hotmail.com>
2025-08-20 14:32:13 +0200

    data importerad

--- backend/src/main/java/com/app/timeline/service/TimelineService.java
+++ backend/src/main/java/com/app/timeline/service/TimelineService.java
@@ -13,6 +13,6 @@ public class TimelineService {
             new TimelineEvent("1510", "", "Sala Silvergruva", "", ""),
             new TimelineEvent("1520", "", "Stockholms blodbad", "Stockholm", ""),
             new TimelineEvent("1544", "", "Vasatidens arvslag", "Uppsala", "")
-        );
+);
     }
 }
